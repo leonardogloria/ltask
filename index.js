514d3c607d40d32c41b0309907d933e8e49ee277
= <=>
2016-06-16 10:39:01 -0300

    Configurações iniciais e express

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.get("/",(req,res) => res.json({status: "LTask OK"}));
+
+app.listen(PORT, () => console.log("LTask rodando!"));
+
